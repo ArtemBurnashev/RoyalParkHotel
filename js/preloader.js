104bfcfa58660c7cf943b3d67be55d0f7fb38a49
ArtemBurnashev <88794381+ArtemBurnashev@users.noreply.github.com>
2021-08-19 23:21:08 +0500

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,7 @@
 const preloader = document.getElementById('preloader');
+window.addEventListener('load', function(){
   setTimeout(function(){
    preloader.classList.add('done');
    setTimeout(function(){ preloader.remove();},600);
-  },2000);
+  },2000);});
 
